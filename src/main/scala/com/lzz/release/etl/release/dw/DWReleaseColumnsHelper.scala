a0fea5c124aee84aad72f17cd34d4870d737f291
SimpleGLee <Lzz533599>
2019-09-26 19:48:47 +0800

    广告投放项目

--- src/main/scala/com/lzz/release/etl/release/dw/DWReleaseColumnsHelper.scala
+++ src/main/scala/com/lzz/release/etl/release/dw/DWReleaseColumnsHelper.scala
@@ -15,6 +15,24 @@ object DWReleaseColumnsHelper {
   def selectDWReleaseCustomerColumns():ArrayBuffer[String]={
     val columns = new ArrayBuffer[String]()
 
+    columns.+=("release_session")
+    columns.+=("release_status")
+    columns.+=("device_num")
+    columns.+=("device_type")
+    columns.+=("sources")
+    columns.+=("channels")
+    columns.+=("get_json_object(exts,'$.idcard') idcard")
+    columns.+=("(cast(date_format(now(),'yyyy') as int) - cast(regexp_extract(get_json_object(exts,'$.idcard'),'(\\\\d{6})(\\\\d{4})',2) as int)) as age")
+    columns.+=("cast(regexp_extract(get_json_object(exts,'$.idcard'),'(\\\\d{16})(\\\\d{1})',2) as int)%2 as gender")
+    columns.+=("get_json_object(exts,'$.area_code') area_code")
+    columns.+=("get_json_object(exts,'$.longitude') longitude")
+    columns.+=("get_json_object(exts,'$.latitude') latitude")
+    columns.+=("get_json_object(exts,'$.matter_id') matter_id")
+    columns.+=("get_json_object(exts,'$.model_code') model_code")
+    columns.+=("get_json_object(exts,'$.model_version') model_version")
+    columns.+=("get_json_object(exts,'$.aid') aid")
+    columns.+=("ct")
+    columns.+=("bdp_day")
 
     columns
   }
--- src/main/scala/com/lzz/release/etl/release/dw/DWReleaseCustomer.scala
+++ src/main/scala/com/lzz/release/etl/release/dw/DWReleaseCustomer.scala
@@ -1,7 +1,10 @@
 package com.lzz.release.etl.release.dw
 
 import com.lzz.release.constant.ReleaseConstant
-import org.apache.spark.sql.SparkSession
+import com.lzz.release.enums.ReleaseStatusEnum
+import com.lzz.release.util.SparkHelper
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.slf4j.{Logger, LoggerFactory}
 
 import scala.collection.mutable.ArrayBuffer
@@ -43,19 +46,78 @@ object DWReleaseCustomer {
       //设置条件 当天数据 获取目标客户：01 lit()-自变量-返回一个列   列与列之间使用===
       val customerReleaseCondition = (col(s"${ReleaseConstant.DEF_PARTITION}") === lit(bdp_day)
         and
-        col(s"${ReleaseConstant.COL_RELEASE_SESSION_STATUS}") === lit()
+        col(s"${ReleaseConstant.COL_RELEASE_SESSION_STATUS}") === lit(ReleaseStatusEnum.CUSTOMER.getCode)
         )
 
       //
+      val customerReleaseDF: DataFrame = SparkHelper.readTableData(sparkSession,ReleaseConstant.ODS_RELEASE_SESSION,customerColumns)
+      //填入条件
+        .where(customerReleaseCondition)
+      //重分区
+        .repartition(ReleaseConstant.DEF_SOURCE_PARTITION)
 
+      println("DWReleaseDF*********************************************************")
 
-    }catch(){
+      // TODO:打印查看结果
+      customerReleaseDF.show(10,false)
 
+      // 目标用户（存储）
+      //SparkHelper.writeTableData(customerReleaseDF,ReleaseConstant.DW_RELEASE_CUSTOMER,saveMode)
+
+    }catch{
+      //错误信息处理
+      case ex:Exception => {
+        logger.error(ex.getMessage,ex)
+      }
+    }finally {
+      //任务处理的时长
+      s"任务处理时长：${appName},bdp_day=${bdp_day},${System.currentTimeMillis()-begin}"
     }
-
-
 
   }
 
+  /**
+    * 投放目标用户
+    * @param appName
+    * @param bdp_day_begin
+    * @param bdp_day_end
+    */
+  def handleJobs(appName:String,bdp_day_begin:String,bdp_day_end:String)={
+    var spark:SparkSession =null
+    try{
+      // 配置Spark参数
+      val conf = new SparkConf()
+        .set("hive.exec.dynamic.partition", "true")
+        .set("hive.exec.dynamic.partition.mode", "nonstrict")
+        .set("spark.sql.shuffle.partitions", "32")
+        .set("hive.merge.mapfiles", "true")
+        .set("hive.input.format", "org.apache.hadoop.hive.ql.io.CombineHiveInputFormat")
+        .set("spark.sql.autoBroadcastJoinThreshold", "50485760")
+        .set("spark.sql.crossJoin.enabled", "true")
+        .setAppName(appName)
+        .setMaster("local[*]")
+      // 创建上下文
+      spark = SparkHelper.createSpark(conf)
+      // 解析参数
+      val timeRange = SparkHelper.rangeDates(bdp_day_begin,bdp_day_end)
+      // 循环参数
+      for(bdp_day <- timeRange){
+        val bdp_date = bdp_day.toString
+        handleReleaseJob(spark,appName,bdp_date)
+      }
+    }catch {
+      case ex:Exception=>{
+        logger.error(ex.getMessage,ex)
+      }
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val appName = "dw_release_job"
+    val bdp_day_begin = "20190924"
+    val bdp_day_end = "20190925"
+    //执行Job
+    handleJobs(appName,bdp_day_begin,bdp_day_end)
+  }
 
 }
